7b957e6377e26099853a4d3826b8affe572ace69
Ruslan Laptiev <lapot.besota87@gmail.com>
2023-05-19 12:41:01 +0200

    /-\

--- Laptiev R. HW 18/index.js
+++ Laptiev R. HW 18/index.js
@@ -1,19 +1,3 @@
-// const container = document.querySelector('#container');
-// const table = document.createElement('table');
-// let count = 1;
-// for (let i = 0; i < 10; i++) {
-//   const tr = document.createElement('tr');
-//   for (let j = 0; j < 10; j++) {
-//     const td = document.createElement('td');
-//     td.innerHTML = count;
-//     tr.append(td);
-//     count++;
-//   }
-//   table.append(tr);
-// }
-
-// container.append(table);
-
 const table = document.createElement('table');
 let count = 1;
 
