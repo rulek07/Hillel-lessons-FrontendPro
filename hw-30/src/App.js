69d9e54087a7a588598ed944d1cdd917f69b8707
Ruslan Laptiev <lapot.besota87@gmail.com>
2023-07-29 15:18:13 +0200

    ready

---
+++ hw-30/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Smiles from './components/smiles/smiles';
+
+function App() {
+  return (
+    <div className="App">
+      <Smiles />
+    </div>
+  );
+}
+
+export default App;
